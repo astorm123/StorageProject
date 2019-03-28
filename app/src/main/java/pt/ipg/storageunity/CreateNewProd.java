f19b4528b426bbe1da1a200eb0466753f9ba5e9a
vitor <vitorcool24@gmail.com>
2019-03-28 13:27:53 +0000

    New Restrictions

--- app/src/main/java/pt/ipg/storageunity/CreateNewProd.java
+++ app/src/main/java/pt/ipg/storageunity/CreateNewProd.java
@@ -26,56 +26,53 @@ public class CreateNewProd extends AppCompatActivity {
             }
         });
 
-    }
+        button = (Button) findViewById(R.id.Save);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText IDprod = findViewById(R.id.IDprod);
+                String SaveID = IDprod.getText().toString();
 
-    public void saveProd(View view){
-        EditText IDprod = findViewById(R.id.IDprod);
-        String SaveID = IDprod.getText().toString();
+                if (SaveID.trim().length()==0){
+                    IDprod.setError(getString(R.string.Error_Message));
+                    IDprod.requestFocus();
 
-            if (SaveID.trim().length()==0){
-                IDprod.setError(getString(R.string.Error_Message));
-                IDprod.requestFocus();
-                return;
-            }
-        EditText Categoria = findViewById(R.id.Categoria);
-        String SaveCategoria = Categoria.getText().toString();
-            if (SaveCategoria.trim().length()==0){
-                Categoria.setError(getString(R.string.Error_Message));
-                Categoria.requestFocus();
-                return;
-            }
-         EditText NameProd = findViewById(R.id.NameProd)   ;
-            String SaveName = NameProd.getText().toString();
-            if (SaveName.trim().length()==0){
-                NameProd.setError(getString(R.string.Error_Message));
-                NameProd.requestFocus();
-                return;
-            }
-         EditText Fornecedor = findViewById(R.id.Fornecedor);
-         String SaveForcenedor = Fornecedor.getText().toString();
-            if (SaveForcenedor.trim().length()==0){
-                Fornecedor.setError(getString(R.string.Error_Message));
-                Fornecedor.requestFocus();
-                return;
-            }
-          EditText Sector = findViewById(R.id.Sector);
-          String SaveSector = Sector.getText().toString();
-            if (SaveSector.trim().length()==0){
-                Sector.setError(getString(R.string.Error_Message));
-                Sector.requestFocus();
-                return;
-            }
-
-            button = (Button) findViewById(R.id.Save);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                    Toast.makeText(CreateNewProd.this,"Criado novo Produto",Toast.LENGTH_LONG).show();
                 }
-            });
+                EditText Categoria = findViewById(R.id.Categoria);
+                String SaveCategoria = Categoria.getText().toString();
+                if (SaveCategoria.trim().length()==0){
+                    Categoria.setError(getString(R.string.Error_Message));
+                    Categoria.requestFocus();
 
+                }
+                EditText NameProd = findViewById(R.id.NameProd)   ;
+                String SaveName = NameProd.getText().toString();
+                if (SaveName.trim().length()==0){
+                    NameProd.setError(getString(R.string.Error_Message));
+                    NameProd.requestFocus();
+
+                }
+                EditText Fornecedor = findViewById(R.id.Fornecedor);
+                String SaveForcenedor = Fornecedor.getText().toString();
+                if (SaveForcenedor.trim().length()==0){
+                    Fornecedor.setError(getString(R.string.Error_Message));
+                    Fornecedor.requestFocus();
+                    
+                }
+                EditText Sector = findViewById(R.id.Sector);
+                String SaveSector = Sector.getText().toString();
+                if (SaveSector.trim().length()==0){
+                    Sector.setError(getString(R.string.Error_Message));
+                    Sector.requestFocus();
+                    return;
+                }
+                finish();
+                Toast.makeText(CreateNewProd.this,"Criado novo Produto",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
+
+
 
 }
