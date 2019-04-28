f65c7f0cef451f2e8a7b5036bf54118931b61c22
vitor <vitorcool24@gmail.com>
2019-04-29 00:07:45 +0100

    Show Information on Edit and Delete Activity

--- app/src/main/java/pt/ipg/storageunity/DeleteProduct.java
+++ app/src/main/java/pt/ipg/storageunity/DeleteProduct.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class DeleteProduct extends AppCompatActivity {
@@ -30,6 +31,35 @@ public class DeleteProduct extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                    EditText Categoria = findViewById(R.id.Categoria);
+                    String SaveCategoria = Categoria.getText().toString();
+                    if (SaveCategoria.trim().length()==0){
+                        Categoria.setError(getString(R.string.Error_Message));
+                        Categoria.requestFocus();
+                        return;
+
+                    }
+                    EditText NameProd = findViewById(R.id.NameProd)   ;
+                    String SaveName = NameProd.getText().toString();
+                    if (SaveName.trim().length()==0){
+                        NameProd.setError(getString(R.string.Error_Message));
+                        NameProd.requestFocus();
+                        return;
+                    }
+                    EditText Fornecedor = findViewById(R.id.Fornecedor);
+                    String SaveForcenedor = Fornecedor.getText().toString();
+                    if (SaveForcenedor.trim().length()==0){
+                        Fornecedor.setError(getString(R.string.Error_Message));
+                        Fornecedor.requestFocus();
+                        return;
+                    }
+                    EditText Sector = findViewById(R.id.Sector);
+                    String SaveSector = Sector.getText().toString();
+                    if (SaveSector.trim().length()==0){
+                        Sector.setError(getString(R.string.Error_Message));
+                        Sector.requestFocus();
+                        return;
+                    }
                 finish();
                 Toast.makeText(DeleteProduct.this,"Product Deleted",Toast.LENGTH_LONG).show();
             }
