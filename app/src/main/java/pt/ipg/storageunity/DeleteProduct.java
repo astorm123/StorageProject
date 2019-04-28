be4354d75c33f9684b23cfb1371a97d165e36489
vitor <vitorcool24@gmail.com>
2019-04-29 00:23:09 +0100

    Delete Activity try 1

--- app/src/main/java/pt/ipg/storageunity/DeleteProduct.java
+++ app/src/main/java/pt/ipg/storageunity/DeleteProduct.java
@@ -34,31 +34,30 @@ public class DeleteProduct extends AppCompatActivity {
                     EditText Categoria = findViewById(R.id.Categoria);
                     String SaveCategoria = Categoria.getText().toString();
                     if (SaveCategoria.trim().length()==0){
-                        Categoria.setError(getString(R.string.Error_Message));
+
                         Categoria.requestFocus();
-                        return;
+
 
                     }
                     EditText NameProd = findViewById(R.id.NameProd)   ;
                     String SaveName = NameProd.getText().toString();
                     if (SaveName.trim().length()==0){
-                        NameProd.setError(getString(R.string.Error_Message));
+
                         NameProd.requestFocus();
-                        return;
+
                     }
                     EditText Fornecedor = findViewById(R.id.Fornecedor);
                     String SaveForcenedor = Fornecedor.getText().toString();
                     if (SaveForcenedor.trim().length()==0){
-                        Fornecedor.setError(getString(R.string.Error_Message));
+
                         Fornecedor.requestFocus();
-                        return;
+
                     }
                     EditText Sector = findViewById(R.id.Sector);
                     String SaveSector = Sector.getText().toString();
                     if (SaveSector.trim().length()==0){
-                        Sector.setError(getString(R.string.Error_Message));
                         Sector.requestFocus();
-                        return;
+
                     }
                 finish();
                 Toast.makeText(DeleteProduct.this,"Product Deleted",Toast.LENGTH_LONG).show();
