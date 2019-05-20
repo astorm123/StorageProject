006a259b7229b21e07e702a2e1dae16130e4706f
vitor <vitorcool24@gmail.com>
2019-05-20 03:51:37 +0100

    New activity Supplier ( almost all functions operational )

--- app/src/main/java/pt/ipg/storageunity/DeleteSup.java
+++ app/src/main/java/pt/ipg/storageunity/DeleteSup.java
@@ -3,12 +3,34 @@ package pt.ipg.storageunity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class DeleteSup extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_sup);
+
+        button = (Button) findViewById(R.id.DeleteSup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Toast.makeText(DeleteSup.this,"Supplier Deleted",Toast.LENGTH_LONG).show();
+            }
+        });
+        
+        button = findViewById(R.id.Cancel);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Toast.makeText(DeleteSup.this, "Operation Canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/pt/ipg/storageunity/Fornecedores.java
+++ app/src/main/java/pt/ipg/storageunity/Fornecedores.java
@@ -2,13 +2,57 @@ package pt.ipg.storageunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Fornecedores extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fornecedores);
+
+        button = findViewById(R.id.AddSup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreatSup();
+            }
+        });
+
+        button = findViewById(R.id.EditSup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditSup();
+            }
+        });
+
+        button =findViewById(R.id.DeleteSup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDeleteSup();
+            }
+        });
+    }
+
+    public void openCreatSup(){
+        Intent intent = new Intent(this,CreatNewSup.class);
+        startActivity(intent);
+    }
+
+    public void openEditSup(){
+        Intent intent = new Intent(this,EditSup.class);
+        startActivity(intent);
+    }
+
+    public void openDeleteSup(){
+        Intent intent = new Intent(this,DeleteSup.class);
+        startActivity(intent);
     }
 }
